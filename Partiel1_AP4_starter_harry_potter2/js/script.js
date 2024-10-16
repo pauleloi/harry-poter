fb8e13920f8076a52bc0715e887949997a50f735
pauleloi <paul-eloi.costrel-de-corainville@student.junia.com>
2024-10-16 10:40:42 +0200

    perso en html

--- Partiel1_AP4_starter_harry_potter2/js/script.js
+++ Partiel1_AP4_starter_harry_potter2/js/script.js
@@ -1,19 +1,40 @@
 
 
+let houses = ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"];
+let container = document.querySelector(".houses");
 
-    let houses = ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]
-    let container = document.querySelector(".houses")
-    for (const house of houses) {
+for (const house of houses) {
+  let div = document.createElement("div");
+  div.classList.add("house-item"); 
+
+  div.addEventListener("click", () => {
+    console.log("test");
+    loadData(house);
+  });
+
+  div.innerHTML = `
+    <img src="./images/logo/${house}.png" alt="${house} logo" />
+  `;
+
+  container.appendChild(div);
+}
+
+
+
+
+    let Characters = ["cho"]
+    let container2 = document.querySelector(".Characters")
+    for (const Character of Characters) {
       let div = document.createElement("div")
   
       div.addEventListener("click", () => {
         console.log("test")
-        loadData(house)
+        loadData(Character)
       })
   
       div.innerHTML = `
-      <img src="./images/logo/${house}.png" alt="" srcset="" />`
+      <img src="./images/logo/${Character}.png" alt="" srcset="" />`
   
       container.appendChild(div)
     }
-    
+  
